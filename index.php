df41732e465fb955aaa0135d725ad7a769cc1b79
Robin AUBERTIN <robin.aubertin.simplon@gmail.com>
2021-10-27 13:32:00 +0200

    init index

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo 'Bla bla bla';
+include 'observer.php';
+
+$observer1 = new ObserverOne();
+$observer2 = new ObserverTwo();
+
+$topic = new Topic();
+$topic->attach($observer1);
+$topic->attach($observer2);
+
+$topic->doSomething();
+echo "<br>";
+$topic->doSomething();
+echo "<br>";
+$topic->doSomething();
+
+echo 'Done';
